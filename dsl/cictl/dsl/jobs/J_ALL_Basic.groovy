ae6ae02dc225636f7ae7f71604dae1d36d299c31
Alexander Schiessl <alexrjs@aol.com>
2016-02-21 13:10:52 +0100

    Experiment to fix a warning;

--- dsl/cictl/dsl/jobs/J_ALL_Basic.groovy
+++ dsl/cictl/dsl/jobs/J_ALL_Basic.groovy
@@ -15,4 +15,4 @@ new DslJobBase(
         name: Constants.S_COMMON_DIR + Constants.S_JOB_SEPARATOR + "J_ALL_BASIC",
         displayName: "ALL: Basic [J,1.0,M]",
         description: "A basic job"
-).build(this as DslFactory)
+).build.call(this as DslFactory)
