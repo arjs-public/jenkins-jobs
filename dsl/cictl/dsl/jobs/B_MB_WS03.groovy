cc201e248fa8375c2d50a1b8f30859943c1bfe68
alexrjs <alexrjs@gmail.com>
2016-02-27 14:46:15 +0100

    Fixed after merge problems;

--- dsl/cictl/dsl/jobs/B_MB_WS03.groovy
+++ dsl/cictl/dsl/jobs/B_MB_WS03.groovy
@@ -14,37 +14,10 @@ folder(Constants.S_SITES_DIR) {
     description('Folder for websites')
 }
 
-folder(Constants.S_SITES_DIR + Constants.S_JOB_SEPARATOR + "CF_ALL_WS03") {
+def folder_name = Constants.S_SITES_DIR + Constants.S_JOB_SEPARATOR + "CF_ALL_WS03"
+folder(folder_name) {
     displayName('v2.arjs.net')
     description('Folder for project v2.arjs.net')
-}
-
-job = new DslJobBase(
-        name: Constants.S_SITES_DIR + Constants.S_JOB_SEPARATOR + "CF_ALL_WS03" + Constants.S_JOB_SEPARATOR + "B_MB_WS03",
-        displayName: "ALL: v2.arjs.net [B,MB,WS03,1.0,T]",
-        description: "A job to build v2.arjs.net (WS03) from master branch (MB)."
-).build.call(this as DslFactory)
-
-job.parameters {
-    booleanParam('DEBUG1', false, 'Enable debugging of script1.')
-}
-
-job.scm {
-    git {
-        remote {
-            name('v2.arjs.net')
-            credentials(Constants.S_BBORG_ALEXRJS)
-            url('git@bitbucket.org:alexrjs/sites-v2.arjs.net.git')
-        }
-        clean()
-        branch('*/master')
-        recursiveSubmodules(false)
-        relativeTargetDir('src')
-    }
-}
-
-job.wrappers {
-    buildName('${GIT_BRANCH}/${GIT_REVISION, length=8}')
 }
 
 String scriptText
@@ -54,15 +27,53 @@ if (Utilities.isWorkingPath) {
 } else {
     scriptText = new File(filePath).getText('UTF-8')
 }
-job.steps {
-    shell(scriptText)
-}
 
-job.publishers {
-    retryBuild {
-        progressiveDelay(14, 45)
-        retryLimit(3)
+def types = ['patch', 'minor', 'major']
+def targets = ['publish', 'pack', 'test']
+def envs = ['ci', 'stage', 'prod']
+envs.each { env ->
+    def u_env = env.toUpperCase()
+    def name = "B_MB_" + u_env + "_WS03"
+    job = new DslJobBase(
+            name: folder_name + Constants.S_JOB_SEPARATOR + name,
+            displayName: u_env + ": v2.arjs.net [B,MB,WS03,1.0,T]",
+            description: "A job to build v2.arjs.net (WS03) from master branch (MB) for '" + env + "'."
+    ).build.call(this as DslFactory)
+
+    job.parameters {
+        choiceParam('environment', [env], 'The environment to use.')
+        choiceParam('type', types, 'The type to use.')
+        choiceParam('target', targets, 'The target to use.')
+        booleanParam('DEBUG1', false, 'Enable debugging of script1.')
     }
-    archiveJunit('src/reports/*')
-}
 
+    job.scm {
+        git {
+            remote {
+                delegate.name('v2.arjs.net')
+                credentials(Constants.S_BBORG_ALEXRJS)
+                url('git@bitbucket.org:alexrjs/sites-v2.arjs.net.git')
+            }
+            clean()
+            branch('*/master')
+            recursiveSubmodules(false)
+            relativeTargetDir('src')
+        }
+    }
+
+    job.wrappers {
+        buildName('${GIT_BRANCH}/${GIT_REVISION, length=8}')
+    }
+
+    job.steps {
+        shell(scriptText)
+    }
+
+    job.publishers {
+        retryBuild {
+            progressiveDelay(14, 45)
+            retryLimit(3)
+        }
+        archiveJunit('src/reports/*')
+    }
+}
